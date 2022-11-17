527410eec2a3caa8352b0c4324189c4c0b4fc502
ProjetHTL <hatanluc0@outlook.fr>
2022-11-17 15:01:16 +0100

    02-second commit

---
+++ ajout_produit.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * 4status de fichier sur git
+ *  -untrack => les fichiers crée mais non suivies par git
+ *  -tracked => fichier déja suivi par git mais non envoyé vers le repository
+ *  -staged => les fichiers suivi enregistré mais non envoyé vers le repository
+ *  -commitred => Tous les fichiers envoyé et non modifié de mon projet
+ * 
+ */
